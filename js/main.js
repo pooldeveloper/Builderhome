1dbab67f7c141111d474bd050273eebf4c1ada78
unknown <pooldeveloper08@gmail.com>
2021-04-28 19:53:11 -0500

    adding scroll with css

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ const navegacionMobil = document.querySelector('.navegacion-mobil');
 const barra = document.querySelector('.barra');
 const enlace = document.querySelectorAll('.enlace');
 
-//Cabezera
-mobilBtn.addEventListener('click',() =>{
-	navegacionMobil.classList.toggle('mostrar-navegacion');
-});
+cargarEventos();
+
+//Cargar eventos
+function cargarEventos(){
+    mobilBtn.addEventListener('click',(mostrarNavegacion));
+} 
+//Funcion mostrarNavegacion
+function mostrarNavegacion(){
+    navegacionMobil.classList.toggle('mostrar-navegacion');
+}
+
 enlace.forEach((enlaces) => {
     enlaces.addEventListener('click', (e) => {
         navegacionMobil.classList.toggle('mostrar-navegacion');
@@ -14,10 +21,3 @@ enlace.forEach((enlaces) => {
         e.target.classList.add('enlace-activo');
     });
 });
-
-//SmoothScroll
-var scroll = new SmoothScroll('a[href*="#"]',{
-	speed: 300
-});
-
-
